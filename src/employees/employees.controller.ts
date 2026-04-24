463819996a08a13e5293722551146dafb332ca66
Jefrin <info.graceter.com>
2026-04-24 21:39:07 +0530

    added DTO validation for employee creation

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
+import { CreateEmployeeDto } from './dto/createEmployee.dto';
 
 @Controller('employees')
 export class EmployeesController {
     constructor(protected readonly service: EmployeesService) { }
 
     @Post()
-    create(@Body() body) {
+    create(@Body() body: CreateEmployeeDto) {
         return this.service.create(body);
     }
 }
